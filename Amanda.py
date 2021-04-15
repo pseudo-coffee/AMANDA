389f7e0960d80e87704c26907d7e5a2e7d86a6b2
Jason Kyle <pseudo-coffee@outlook.com>
2021-04-15 17:01:11 +0000

    Added Amanda.py and a few functions as base code

---
+++ Amanda.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from gtts import gTTS
+
+import datetime
+import pickle
+import os
+import time
+import pytz
+import random
+import smtplib
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import subprocess
+import requests
+import json
+import random
+import string
+import playsound
+
+SCOPES = ['https://www.googleapis.com/calendar.readonly']
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+MONTHS = ["january", "febuary", "march", "april", "may", "june" ,"july" ,"august", "september", "october", "november", "december"]
+OS = ['posix', 'nt']
+
+
+class Amanda(object):
+	def __init__(self):
+		self.chars = string.ascii_letters + string.digits
+
+	def make_filename(self):
+		"""
+		Generate a string with random characters and use it as
+		the filename in speak().
+		"""
+		length = random.randint(10, 20)
+		i = 0
+		filename = ''
+		while i != length:
+			char = ''
+			char = random.choice(self.chars)
+			filename += char
+			i += 1
+
+		return filename
+
+	def speak(self, text):
+		"""
+		Generate a voice. This will interact with the user.
+		"""
+		tts = gTTS(text=text, lang='en')
+		filename = self.make_filename() + '.mp3'
+		tts.save(filename)   # The .mp3 file will be deleted later.
+		playsound.playsound(filename)
+
+		if os.name == OS[0]:
+			os.system(f'rm -rf {filename}')
+		else:
+			os.system(f'del /f {filename}')
+
+	@staticmethod
+	def take_command():
+		"""
+		Take voice input from the microphone and convert it into 
+		text. 
+		"""
+		r = sr.Recognizer()  #  it helps to detect audio from the microphone.
+		with sr.Microphone() as source:
+			print('Listening...')
+			r.pause_threshold = 1
+			audio = r.listen(source)
+
+		try:
+			print('Proccessing...')
+			query - r.recognize_google(audio, languages='en-in')
+			print(f'User said: {query}')
+
+		except Exception as e:
+			print(e)
+			print('Could not recognise...')
+			return None
+		return query
+
+	def authenticate_google():
+		creds = None
+		if os.path.exists('token.pickle'):
+			with open('token.pickle', 'rb') as token:
+				creds = pickle.load(token)
+
+		# if there are no (valid) credentials available, let the user log in.
+		if not creds or not creds.valid:
+			if creds and creds.expired and creds.refresh_token():
+				creds.refresh(Request())
+			else:
+				flow = InstalledAppFlow.from_client_secrects_file(
+					'credentials.json', SCOPES)
+				creds = flow.run_local_server(port=0)
+
+			# save the credentials for the next run
+			with open('token.pickle', 'wb') as token:
+				pickle.dump(creds, token)
+
+		service = build('calendar', 'v3', credentials=creds)
+		return service
+
+	@staticmethod
+	def get_events(day, service):
+		date = datetime.datetime.combine(day, datetime.datetime.min.time())  # the min time on that day.
+		end_date = datetime.datetime.combine(day, datetime.datetime.max.time())  # the max time on that date.
+		utc = pytz.UTC
+		date = date.astimezone(utc)
+		end_date = end_date.astimezone(utc)
+
+		events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat(), singleEvents=True, orderBy='startTime').execute()
+		events = events_result.get('items', [])
+
+		if not events:
+			speak('No upcoming events found.')
+		else:
+			speak(f'You have {len(events)} events on this day.')
+
+			for event in events:
+				start = event['start'].get('dateTime', event['start'].get('date'))
+				print(start, event['summary'])
+				start_time = str(start.split('T')[1].split('+')[0].split(':')[0])
+
+				if int(start_time.split(":")[0]) < 12:
+					start_time = start_time + "AM"
+				else:
+					start_time = str(int(start_time.split(":")[0])-12)
+					start_time = start_time + "PM"
+				speak(event['summary'] + 'at' + start_time)
+
+	@staticmethod
+	def get_date(text):
+		"""
+		Get the date from the user.
+		"""
+		text = text.lower()
+		today = datetime.date.today()
+
+		if text.count("today")>0:
+			return today
+
+		day = -1
+		day_of_week = -1
+		month = -1
+		year = today.year
+
+		for word in text.split():
+			if word in MONTHS:
+				month = MONTHS.index(word) + 1
+
+			elif word in DAYS:
+				day_of_week = DAYS.index(word)
+
+			elif word.isdigit():
+				day = int(word)
+			else:
+				for ext in DAY_EXTENTIONS:
+					found = word.find(ext)
+					if found > 0:
+						try:
+							day = int(word[:found])
+						except:
+							pass
+
+		if month < today.month and month != -1:
+			year = year + 1
+
+		if month == -1 and day != -1:
+			if day < today.day:
+				month = today.month + 1
+			else:
+				month = today.month
+
+		if month == -1 and day == -1 and day_of_week == -1:
+			current_day_of_week = today.weekday()
+			dif = day_of_week - current_day_of_week
+
+			if dif < 0:
+				dif += 7
+				if text.count("next") >= 1:
+					dif += 7
+			return today + datetime.date(month=month, day=day, year=year)
+
+
+if __name__ == '__main__':
+	pass
